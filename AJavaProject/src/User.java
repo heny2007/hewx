a2adff84a32f238a2dcfa571837bf086f4f141a6
hewanxian <heny2007@163.com>
2016-12-12 17:05:45 +0800

    增加bean User

---
+++ AJavaProject/src/User.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7020619477594468968L;
+	private String id;
+	private String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
