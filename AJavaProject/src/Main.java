2e0fc9eb3bc3c72126c84ccb075dd7b8998f598e
hewanxian <heny2007@163.com>
2016-12-15 19:32:31 +0800

    DDDDD

--- AJavaProject/src/Main.java
+++ AJavaProject/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 		print(new User());
 	}
 
-	public static void print(User ubs) {
-		System.out.println(ubs.toString());
+	public static void print(User EE) {
+		System.out.println(EE.toString());
 		
 	}
 }
