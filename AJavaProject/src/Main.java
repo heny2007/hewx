feadcd784e5bfe22eaa655fb5c05e16c20cafd67
hewanxian <heny2007@163.com>
2016-12-15 20:30:14 +0800

    ddd

--- AJavaProject/src/Main.java
+++ AJavaProject/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 		print(new User());
 	}
 
-	public static void print(User a) {
-		System.out.println(a.toString());
+	public static void print(User ddd) {
+		System.out.println(ddd.toString());
 	}
 }
